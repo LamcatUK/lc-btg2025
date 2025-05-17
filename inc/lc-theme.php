76e3f389cbcb6ed94c07dac52a4d02901d6b1cf5
LamcatUK <git@lamcat.co.uk>
2025-05-17 16:52:30 +0000

    removed testes

--- inc/lc-theme.php
+++ inc/lc-theme.php
@@ -12,8 +12,8 @@ defined( 'ABSPATH' ) || exit;
 require_once LC_THEME_DIR . '/inc/lc-utility.php';
 require_once LC_THEME_DIR . '/inc/lc-blocks.php';
 require_once LC_THEME_DIR . '/inc/lc-news.php';
-require_once LC_THEME_DIR . '/inc/lc-posttypes.php';
-require_once LC_THEME_DIR . '/inc/lc-taxonomies.php';
+// require_once LC_THEME_DIR . '/inc/lc-posttypes.php';
+// require_once LC_THEME_DIR . '/inc/lc-taxonomies.php';
 
 if ( ! defined( 'LC_COLOUR_PALETTE' ) ) {
 	define(
